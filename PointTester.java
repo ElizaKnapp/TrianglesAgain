7a205684b3880f503ba0cf938499fdfcd9ceab21
ElizaKnapp <eknapp20@stuy.edu>
2020-10-29 22:23:36 -0400

    get area and perimeter are tested

--- PointTester.java
+++ PointTester.java
@@ -1,5 +1,6 @@
 public class PointTester {
   public static void main(String[] args) {
+    /*
     Point t1 = new Point(3, 4);
     System.out.println(t1.getX());
 
@@ -9,5 +10,16 @@ public class PointTester {
     Point t3 = new Point(0,0);
     System.out.println(t3.equals(t2));
     System.out.println(t3.equals(t1));
+    */
+
+    //scalene
+    Point t1 = new Point(0, 4);
+    Point t2 = new Point(0, 0);
+    Point t3 = new Point(3, 0);
+    Triangle yay = new Triangle(t1, t2, t3);
+    System.out.println(yay.getPerimeter());//12
+    System.out.println(yay.getArea());//6
+
+
   }
 }
